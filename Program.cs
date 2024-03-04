e023f5ed0c2ba1cff3ecda862a3bb9157b2e5457
Joshua.Samuel <Joshua.Samuel@BP-12011083>
2024-03-04 13:34:43 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Collections.Generic;
+namespace dictionary
+{
+    class Example
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> d = new Dictionary<int, string>();
+            d.Add(1, "Harry");
+            d.Add(2, "Sally");
+            d.Add(3, "Clarke");
+            d.Add(4, "James");
+            d.Add(5, "Emma");
+            d.Add(6, "Susan");
+            Console.WriteLine("Dictionary elements are as follows:");
+            foreach (KeyValuePair<int, string> i in d)
+            {
+                Console.WriteLine("Key: {0}     Value: {1}", i.Key, i.Value);
+            }
+        }
+    }
+}
